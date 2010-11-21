29d1b68cd179719189f71ff12464eee236eb9640
Jessica McKellar <jesstess@mit.edu>
2010-11-20 21:30:20 -0500

    Add 'serial' as an effect order option.
    Also make the effect order a command line option.

--- wall/wall.py
+++ wall/wall.py
@@ -75,13 +75,16 @@ class Wall64(object):
         return self.chain[pixel]
 
 class WatchAndEffect(object):
-    def __init__(self, wall, nocam=False):
+    def __init__(self, wall, nocam=False, effect_order="random"):
         self.wall = wall
         self.nocam = nocam
         if not self.nocam:
             self.watcher = Watcher()
         else:
             self.watcher = DebugWatcher()
+        self.effect_order = effect_order
+        if self.effect_order:
+            self.current_effect = 0
     
     def run(self):
         while 1:
@@ -99,12 +102,28 @@ class WatchAndEffect(object):
             elif vector[0] == 0:
                 speed = random.random() / 8
                 direction = vector[1]
-            # choose a random effect
-            effect = random.choice(Effects)
+
+            effect = self.pick_effect()
             effect = effect(self.wall, speed=speed, direction=direction)
             print effect
             effect.run()
             self.wall.clear(True)
+
+    def pick_effect(self):
+        """
+        Returns an Effect (sub)class based on the effect_order.
+        """
+        if self.effect_order == "serial":
+            effect = Effects[self.current_effect]
+            self.current_effect += 1
+            if self.current_effect > len(Effects) - 1:
+                self.current_effect = 0
+            return effect
+        elif self.effect_order == "random":
+            return random.choice(Effects)
+        else:
+            print "Unknown effect order: choosing random"
+            return random.choice(Effects)
             
 def run(opts, args):
     if opts['dummy']:
@@ -117,7 +136,8 @@ def run(opts, args):
     for clear_it in range(10):
         wall.clear(True)
         time.sleep(.1)
-    waf = WatchAndEffect(wall, nocam=opts['nocam'])
+    waf = WatchAndEffect(wall, nocam=opts['nocam'],
+                         effect_order=opts['effect_order'])
     waf.run()
 
 def get_args():
@@ -133,12 +153,16 @@ def get_args():
                         help="enable GUI")
     parser.add_option("-C", "--crash", action="store_true",
                         help="Disable crash protection")
+    parser.add_option("-o", "--order", action="store", dest="effect_order",
+                      help="Specify the effects order: one of 'random' or 'serial'")
+
     dport = '__unknown__'
     if sys.platform.startswith('linux'):
         dport = '/dev/ttyUSB0'
     elif sys.platform.startswith('darwin'):
         dport = '/dev/tty.usbserial-A800ewuP'
-    parser.set_defaults(port=dport, debug=False, dummy=False, gui=False, crash=False, nocam=False)
+    parser.set_defaults(port=dport, debug=False, dummy=False, gui=False,
+                        crash=False, nocam=False, effect_order="random")
     (opts, args) = parser.parse_args()
     opts = eval(str(opts))
     return opts, args
